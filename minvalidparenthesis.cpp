72931cefbfe7411ddeeff5b662c7f7d2261d86d0
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-05-20 15:30:50 +0530

    min valid parenthesis

---
+++ minvalidparenthesis.cpp
@@ -0,0 +1,23 @@
+/*leetcode
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<int>st;
+        int n=s.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                st.push(s[i]);
+            }else{
+                if(!st.empty()){
+                    st.pop();
+                }
+                else{
+                    count++;
+                }
+            }
+        }
+        return st.size()+count;
+    }
+};
+*/
