12e3d0f976a1486b26b13900c6968769555fdc2e
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-05-20 14:46:23 +0530

    validparenthesis

---
+++ validparenthesis.cpp
@@ -0,0 +1,27 @@
+/* gfg 
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+        stack<char> st;
+        int n = s.size();
+        for(int i=0; i<n; i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                if((st.top()=='(' && s[i]==')') ||
+                   (st.top()=='{' && s[i]=='}') ||
+                   (st.top()=='[' && s[i]==']')){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};*/
