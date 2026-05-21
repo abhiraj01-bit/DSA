172465846f0a0af5f3ace0e74c18dce3034da076
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-05-21 17:17:39 +0530

    top k element

---
+++ top k element.cpp
@@ -0,0 +1,24 @@
+/*leetcode
+class Solution {
+public:
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        vector<int> arr;
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        for(auto a : mp){
+            pq.push({a.second, a.first});
+            if(pq.size() > k){
+                pq.pop();
+            }
+        }
+        while(!pq.empty()){
+            arr.push_back(pq.top().second);
+            pq.pop();
+        }
+        return arr;
+    }
+};*/
