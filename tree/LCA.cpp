5ed2f074092ed4183c5599a7ae25e34dd324c687
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-05-26 22:58:09 +0530

    LCA

---
+++ tree/LCA.cpp
@@ -0,0 +1,32 @@
+/* leetcode
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+/*class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL){
+            return NULL;
+        }
+        if(root==p||root==q){
+            return root;
+        }
+        TreeNode* sl=lowestCommonAncestor(root->left,p,q);
+        TreeNode* sr=lowestCommonAncestor(root->right,p,q);
+        if(sl&&sr){
+            return root;
+        }
+        if(sl==NULL){
+            return sr;
+        }
+        else{
+            return sl;
+        }
+    }
+};*/
