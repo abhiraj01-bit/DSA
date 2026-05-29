614b2a5f1f4c52ccd30a7072e7d54fbc08453ba5
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-05-29 10:01:38 +0530

    balanced bst

---
+++ tree/balanced bst.cpp
@@ -0,0 +1,28 @@
+/*leetcode 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/*class Solution {
+public:
+TreeNode* bbst(vector<int>& nums,int st,int end){
+    if(st>end){
+        return NULL;
+    }
+    int mid=st+(end-st)/2;
+    TreeNode* root=new TreeNode(nums[mid]);
+   root->left= bbst(nums,st,mid-1);
+    root->right=bbst(nums,mid+1,end);
+    return root;
+};
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return bbst(nums,0,nums.size()-1);
+    }
+};*/
