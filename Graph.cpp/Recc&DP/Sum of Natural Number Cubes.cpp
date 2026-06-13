bc83c5e59ce3e03830936b75ab006d394e54eaa7
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-06-13 14:37:02 +0530

    Sum of Natural Number Cubes

---
+++ Graph.cpp/Recc&DP/Sum of Natural Number Cubes.cpp
@@ -0,0 +1,13 @@
+/*
+// User function template for C++
+
+class Solution {
+  public:
+    int sumOfSeries(int n) {
+        // code here
+        if(n==1){
+            return 1;
+        }
+        return pow(n,3)+ sumOfSeries(n-1);
+    }
+};*/
