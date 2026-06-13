9d0403470f26376794690afb810adf6c4a51d85c
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-06-13 15:21:57 +0530

    reversal

---
+++ Graph.cpp/Recc&DP/reversal.cpp
@@ -0,0 +1,12 @@
+/*class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n=s.size();
+        int i=0;
+        int j=n-1;
+        for(int i=0;i<n/2;i++){
+            swap(s[i],s[j]);
+            j--;
+        }
+    }
+};*/
