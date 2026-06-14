a1573ec06c961bc1a4e75170b067e8cd05ed63fa
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-06-14 09:22:14 +0530

    climbing stairs

---
+++ Graph.cpp/Recc&DP/climbing stairs.cpp
@@ -0,0 +1,24 @@
+/*
+class Solution {
+public:
+int cs(int n,vector<int>&dp){
+     if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+       dp[n]= cs(n-1,dp)+cs(n-2,dp);
+       return dp[n];
+}
+    int climbStairs(int n) {
+       vector<int>dp(n+1,-1);
+       return cs(n,dp);
+    }
+};*/
