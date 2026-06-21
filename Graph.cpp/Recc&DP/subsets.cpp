9601d6351d3ff7e712bc784b4b5185422488ca08
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-06-21 21:43:49 +0530

    subset

--- Graph.cpp/Recc&DP/subsets.cpp
+++ Graph.cpp/Recc&DP/subsets.cpp
@@ -1,20 +1,22 @@
 /*
 class Solution {
-public:
-void sub(vector<int>&nums,vector<vector<int>>&ans,vector<int>&curr,int i){
-    if(i==nums.size()){
+  public:
+void solve(vector<int>&arr,vector<vector<int>>&ans,vector<int>&curr,int i){
+    if(i==arr.size()){
         ans.push_back(curr);
         return;
     }
-    sub(nums,ans,curr,i+1);
-    curr.push_back(nums[i]);
-    sub(nums,ans,curr,i+1);
-    curr.pop_back(); //isko hamesha backtrack ke liye
+    solve(arr,ans,curr,i+1);
+    curr.push_back(arr[i]);
+    solve(arr,ans,curr,i+1);
+    curr.pop_back();
 }
-    vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> subsets(vector<int>& arr) {
+        // code here
         vector<vector<int>>ans;
         vector<int>curr;
-        sub(nums,ans,curr,0);
+        solve(arr,ans,curr,0);
         return ans;
     }
+};
 };*/
