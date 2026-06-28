f1f409fb2e9d8e3864026ab6835d1fef4e9ae7f4
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-06-28 18:10:32 +0530

    mid del

---
+++ Graph.cpp/Recc&DP/remove stars.cpp
@@ -0,0 +1,28 @@
+/*class Solution {
+public:
+    string removeStars(string s) {
+        string ans;
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='*'){
+                if(!st.empty()){
+                    st.pop();
+                }
+            }
+            else{
+                st.push(s[i]);
+            } 
+        }
+        if(st.empty()){
+                return "";
+            }
+            else{
+                while(!st.empty()){
+                    ans.push_back(st.top());
+                    st.pop();
+                }
+            }
+            reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};*/
