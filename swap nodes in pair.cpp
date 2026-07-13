02e3eb84460e54c322fa7eaa39ef68874aac4c80
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-07-13 20:00:34 +0530

    rec swap pair

--- swap nodes in pair.cpp
+++ swap nodes in pair.cpp
@@ -25,4 +25,35 @@ public:
 
         return dummy->next;
     }
-};*/
+};
+BY RECURSION 
+class Solution {
+public:
+    void swap(Node* temp, Node* prev) {
+        if (temp == NULL || temp->next == NULL)
+            return;
+
+        Node* first = temp;
+        Node* second = first->next;
+
+        first->next = second->next;
+        second->next = first;
+        prev->next = second;
+
+        // Recurse for the remaining list
+        swap(first->next, first);
+    }
+
+    Node* pairwiseSwap(Node* head) {
+        if (head == NULL || head->next == NULL)
+            return head;
+
+        Node dummy(0);
+        dummy.next = head;
+
+        swap(head, &dummy);
+
+        return dummy.next;
+    }
+};
+*/
