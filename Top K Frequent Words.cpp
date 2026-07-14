ab9ada6c1af8da6a7577fdf438ab67f86f4e4de2
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-07-14 20:36:23 +0530

    top k frequent words

--- Top K Frequent Words.cpp
+++ Top K Frequent Words.cpp
@@ -1,25 +1,40 @@
-/*class Solution {
+/*class cmp {
+public:
+    bool operator()(pair<int, string>& a, pair<int, string>& b) {
+        if (a.first == b.first)
+            return a.second < b.second;   
+        return a.first > b.first;       
+    }
+};
+
+class Solution {
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
-        unordered_map<string,int>mp;
-         vector<string>arr;
-        for(int i=0;i<words.size();i++){
-            mp[words[i]]++;
+
+        unordered_map<string, int> mp;
+        vector<string> arr;
+
+        for (auto &word : words)
+            mp[word]++;
+
+        priority_queue<pair<int, string>,
+                       vector<pair<int, string>>,
+                       cmp> pq;
+
+        for (auto &a : mp) {
+            pq.push({a.second, a.first});
+
+            if (pq.size() > k)
+                pq.pop();
         }
-        auto cmp=[](pair<int,string>&a,pair<int,string>&b){
-            if(a.first==b.first){
-                return a.second>b.second;
-            }
-            return a.first<b.first;
-        };
-        priority_queue<pair<int,string>,vector<pair<int,string>>,decltype(cmp)>pq(cmp);
-        for(auto a:mp){
-            pq.push({a.second,a.first});
-        }
-        for(int i=0;i<k;i++){
+
+        while (!pq.empty()) {
             arr.push_back(pq.top().second);
             pq.pop();
         }
+
+        reverse(arr.begin(), arr.end());
+
         return arr;
     }
 };*/
