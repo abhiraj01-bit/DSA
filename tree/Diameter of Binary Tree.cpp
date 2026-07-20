8e13a3569089917529f3208a0c9d7ebe438e3ac9
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-07-20 11:06:19 +0530

    diameter of a tree

--- tree/Diameter of Binary Tree.cpp
+++ tree/Diameter of Binary Tree.cpp
@@ -1,32 +1,36 @@
-/*
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-/*class Solution {
-public:
-int height(TreeNode* root){
-    if(root==NULL){
-        return 0;
+/*/*
+Definition for Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
     }
-    int lefth=height(root->left);
-    int righth=height(root->right);
-    return max(lefth,righth)+1;
-}
-    int diameterOfBinaryTree(TreeNode* root) {
+};
+*/
+
+/*class Solution {
+  public:
+  int solve(Node* root){
+      if(root==NULL){
+          return 0;
+      }
+      int left=solve(root->left);
+      int right=solve(root->right);
+      return max(left,right)+1;
+  }
+    int diameter(Node* root) {
+        // code here
         if(root==NULL){
             return 0;
         }
-        int leftt=diameterOfBinaryTree(root->left);
-        int rightt=diameterOfBinaryTree(root->right);
-        int currh=height(root->left)+height(root->right);
-        return max(currh,max(leftt,rightt));
+        int l=diameter(root->left);
+        int r=diameter(root->right);
+        int curr=solve(root->left)+solve(root->right);
+        return max(curr,max(l,r));
     }
 };*/
