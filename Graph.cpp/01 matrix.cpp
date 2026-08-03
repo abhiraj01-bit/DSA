db2fdb2199ef4fd10decc3e4bbcc290e703199fd
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-08-03 19:02:44 +0530

    01 matrix ms bfs

--- Graph.cpp/01 matrix.cpp
+++ Graph.cpp/01 matrix.cpp
@@ -1,18 +1,17 @@
-/*leetcode
-class Solution {
+/*class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        queue<pair<int,int>>q;
         int n=mat.size();
         int m=mat[0].size();
-        int dx[]={-1,1,0,0};
-        int dy[]={0,0,1,-1};
         vector<vector<int>>dist(n,vector<int>(m,-1));
+        queue<pair<int,int>>q;
+        int dx[]={-1,1,0,0};
+        int dy[]={0,0,-1,1};
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
-                    dist[i][j]=0;
-                    q.push({i,j});
+                  dist[i][j]=0;
+                  q.push({i,j});
                 }
             }
         }
