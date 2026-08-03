d8fd113eac0793717d97cecaf0a2c6ffc33f3338
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-08-03 19:38:19 +0530

    rotten oranges

--- Graph.cpp/rotten orange.cpp
+++ Graph.cpp/rotten orange.cpp
@@ -1,10 +1,11 @@
-/*leetcode
-class Solution {
+/*class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int,int>>q;
         int n=grid.size();
         int m=grid[0].size();
+        queue<pair<int,int>>q;
+        int dx[]={-1,1,0,0};
+        int dy[]={0,0,-1,1};
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
@@ -13,23 +14,21 @@ public:
             }
         }
         int time=0;
-        int dx[]={-1,1,0,0};
-        int dy[]={0,0,1,-1};
         while(!q.empty()){
             int sz=q.size();
-           for(int i=0;i<sz;i++){
-             auto[x,y]=q.front();
-            q.pop();
-            for(int k=0;k<4;k++){
-                int nx=x+dx[k];
-                int ny=y+dy[k];
-                if(nx>=0&&nx<n&&ny>=0&&ny<m&&grid[nx][ny]==1){
-                    grid[nx][ny]=2;
-                    q.push({nx,ny});
-                }
+            for(int i=0;i<sz;i++){
+                auto[x,y]=q.front();
+                 q.pop();
+                 for(int j=0;j<4;j++){
+                    int nx=x+dx[j];
+                    int ny=y+dy[j];
+                    if(nx>=0&&nx<n&&ny>=0&&ny<m&&grid[nx][ny]==1){
+                        grid[nx][ny]=2;
+                        q.push({nx,ny});
+                    }
+                 }
             }
-           }
-           if(!q.empty()) time++;;
+            if(!q.empty()) time++;
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
