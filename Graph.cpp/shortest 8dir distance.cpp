1a1da9030d89979cb793f0ad584e8737b5353f77
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-08-03 20:13:26 +0530

    shortest path in binary matrix

--- Graph.cpp/shortest 8dir distance.cpp
+++ Graph.cpp/shortest 8dir distance.cpp
@@ -1,34 +1,37 @@
-/*leetcode
-class Solution {
+/*class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        queue<pair<int,int>>q;
-        int dist=1;
         int n=grid.size();
-        if(grid[0][0]||grid[n-1][n-1]){
+        int m=grid[0].size();
+        if(grid[0][0] == 1 || grid[n-1][m-1] == 1){
             return -1;
         }
+        int dx[]={1,1,0,-1,-1,-1,0,1};
+        int dy[]={0,-1,-1,-1,0,1,1,1};
+        queue<pair<int,int>>q;
         q.push({0,0});
-        int dx[]={-1,-1,0,1,1,1,0,-1};
-        int dy[]={0,1,1,1,0,-1,-1,-1};
+        grid[0][0]=1;
+        int dist=1;
         while(!q.empty()){
             int sz=q.size();
             for(int i=0;i<sz;i++){
-                auto[x,y]=q.front();
-                if(x==n-1&&y==n-1){
+                auto [x,y]=q.front();
+                if(x==n-1&&y==m-1){
                     return dist;
                 }
                 q.pop();
-                for(int k=0;k<8;k++){
-                    int nx=x+dx[k];
-                    int ny=y+dy[k];
-                    if(nx>=0&&nx<n&&ny>=0&&ny<n&&grid[nx][ny]==0){
+                for(int j=0;j<8;j++){
+                    int nx=x+dx[j];
+                    int ny=y+dy[j];
+                    if(nx>=0&&nx<n&&ny>=0&&ny<m&&grid[nx][ny]==0){
                         grid[nx][ny]=1;
                         q.push({nx,ny});
                     }
                 }
             }
-            dist++;
+            if(!q.empty()){
+                dist++;
+            }
         }
         return -1;
     }
