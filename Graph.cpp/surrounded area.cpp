1f1094dc6f96e18756cdd14c4dcdd545626dd142
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-08-10 12:51:30 +0530

    sorrounded region

--- Graph.cpp/surrounded area.cpp
+++ Graph.cpp/surrounded area.cpp
@@ -1,44 +1,33 @@
-/*gfg
-class Solution {
-  public:
-  void dfs(vector<vector<char>>& grid,int i,int j,vector<vector<bool>>&vis,int n,int m){
-      if(i>=n||i<0||j>=m||j<0||grid[i][j]=='X'||vis[i][j]){
-          return;
-      }
-      vis[i][j]=true;
-      dfs(grid,i-1,j,vis,n,m);
-      dfs(grid,i+1,j,vis,n,m);
-      dfs(grid,i,j+1,vis,n,m);
-      dfs(grid,i,j-1,vis,n,m);
-  }
-    void fill(vector<vector<char>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
+/*class Solution {
+public:
+void dfs(int x,int y,vector<vector<char>>& board,vector<vector<bool>>&vis){
+    int n=board.size();
+    int m=board[0].size();
+    if(x<0||x>=n||y<0||y>=m||vis[x][y]||board[x][y]=='X'){
+        return;
+    }
+    vis[x][y]=true;
+    dfs(x-1,y,board,vis);
+    dfs(x+1,y,board,vis);
+    dfs(x,y-1,board,vis);
+    dfs(x,y+1,board,vis);
+}
+    void solve(vector<vector<char>>& board) {
+        int n=board.size();
+        int m=board[0].size();
         vector<vector<bool>>vis(n,vector<bool>(m,false));
-        for(int i=0;i<n;i++){
-            if(!vis[i][0]&&grid[i][0]=='O'){
-                dfs(grid,i,0,vis,n,m);
-            }
+        for(int i=0;i<m;i++){
+            dfs(0,i,board,vis);
+            dfs(n-1,i,board,vis);
         }
         for(int i=0;i<n;i++){
-            if(!vis[i][m-1]&&grid[i][m-1]=='O'){
-                dfs(grid,i,m-1,vis,n,m);
-            }
-        }
-        for(int j=0;j<m;j++){
-            if(!vis[0][j]&&grid[0][j]=='O'){
-                dfs(grid,0,j,vis,n,m);
-            }
-        }
-        for(int j=0;j<m;j++){
-            if(!vis[n-1][j]&&grid[n-1][j]=='O'){
-                dfs(grid,n-1,j,vis,n,m);
-            }
+            dfs(i,0,board,vis);
+            dfs(i,m-1,board,vis);
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j]&&grid[i][j]=='O'){
-                    grid[i][j]='X';
+                if(!vis[i][j]){
+                    board[i][j]='X';
                 }
             }
         }
