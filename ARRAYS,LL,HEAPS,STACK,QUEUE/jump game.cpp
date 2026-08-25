fefadecba74b5330ff542ad16f4e6bf79860eaba
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-08-25 21:22:37 +0530

    jump game

---
+++ ARRAYS,LL,HEAPS,STACK,QUEUE/jump game.cpp
@@ -0,0 +1,26 @@
+/*class Solution {
+public:
+    bool solve(int i, vector<int>& nums, int n,vector<int>&dp) {
+
+        if(i >= n) {
+            return true;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        for(int j = 1; j <= nums[i]; j++) {
+
+            if(solve(i + j, nums, n,dp)) {
+               return dp[i]=1;
+            }
+        }
+
+        return dp[i]=0;
+    }
+
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n-1,-1);
+        return solve(0, nums, nums.size() - 1,dp);
+    }
+};*/
