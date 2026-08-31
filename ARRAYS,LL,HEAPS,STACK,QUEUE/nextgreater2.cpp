c5b711bc314670455324d6a374b77b0ae9a1fe54
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-08-31 21:21:14 +0530

    next greater 2

--- ARRAYS,LL,HEAPS,STACK,QUEUE/nextgreater2.cpp
+++ ARRAYS,LL,HEAPS,STACK,QUEUE/nextgreater2.cpp
@@ -28,3 +28,50 @@ public:
         return ans;
     }
 };*/
+/*class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> nums2(2 * n);
+
+        for(int i = 0; i < n; i++) {
+            nums2[i] = nums[i];
+            nums2[i + n] = nums[i];
+        }
+        vector<int> ans2(2 * n, -1);
+
+        stack<int> s;
+
+        for(int i = nums2.size() - 1; i >= 0; i--) {
+
+            if(s.empty()) {
+                ans2[i] = -1;
+            }
+            else {
+
+                while(!s.empty()) {
+
+                    if(s.top() > nums2[i]) {
+                        ans2[i] = s.top();
+                        break;
+                    }
+
+                    s.pop();
+                }
+                if(s.empty()) {
+                    ans2[i] = -1;
+                }
+            }
+
+            s.push(nums2[i]);
+        }
+        vector<int> ans;
+
+        for(int i = 0; i < n; i++) {
+            ans.push_back(ans2[i]);
+        }
+
+        return ans;
+    }
+};*/
