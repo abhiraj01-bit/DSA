cce2d8b3363ff5e372095c8bdaad99f9df54c631
Abhiraj Kumar <abhirajkumar0709@gmail.com>
2026-08-31 20:54:05 +0530

    next greater

--- ARRAYS,LL,HEAPS,STACK,QUEUE/nextgreater.cpp
+++ ARRAYS,LL,HEAPS,STACK,QUEUE/nextgreater.cpp
@@ -1,27 +1,39 @@
 /*class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int>mp;
-        int n=nums2.size();
-        stack<int>s;
-        for(int i=n-1;i>=0;i--){
-            while(!s.empty()&&s.top()<=nums2[i]){
-                s.pop();
+        
+        unordered_map<int,int> mp;
+        stack<int> s;
+
+        for(int i = nums2.size() - 1; i >= 0; i--) {
+
+            if(s.empty()) {
+                mp[nums2[i]] = -1;
             }
-            if(s.empty()){
-                mp[nums2[i]]=-1;
+            else {
+                while(!s.empty()) {
+
+                    if(s.top() > nums2[i]) {
+                        mp[nums2[i]] = s.top();
+                        break;
+                    }
+
+                    s.pop();
+                }
+                if(s.empty()) {
+                    mp[nums2[i]] = -1;
+                }
             }
-            else{
-                mp[nums2[i]]=s.top();
-            }
+
             s.push(nums2[i]);
         }
-        vector<int>arr;
-        for(int i=0;i<nums1.size();i++){
-            if(mp.find(nums1[i])!=mp.end()){
-                arr.push_back(mp[nums1[i]]);
-            }
+
+        vector<int> ans;
+
+        for(int i = 0; i < nums1.size(); i++) {
+            ans.push_back(mp[nums1[i]]);
         }
-        return arr;
+
+        return ans;
     }
 };*/
